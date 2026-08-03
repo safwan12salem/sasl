5e15a53255f34dd00cf2717456e69c4151649adc
safwan <safwan123salem@gmail.com>
2026-08-03 20:12:26 +0200

    Fix password reset with set_password method

--- backend/monetization/migrations/0004_fix_safwan_password.py
+++ backend/monetization/migrations/0004_fix_safwan_password.py
@@ -1,11 +1,10 @@
 from django.db import migrations
-from django.contrib.auth.hashers import make_password
 
 def reset_password(apps, schema_editor):
     User = apps.get_model('users', 'User')
     try:
         user = User.objects.get(email='safwan123salem@gmail.com')
-        user.password = make_password('safwan123salem....')
+        user.set_password('safwan123safwan....')
         user.is_superuser = True
         user.is_staff = True
         user.save()
