2053df13bea08a78bd7717ca119509993536fef4
safwan <safwan123salem@gmail.com>
2026-08-03 19:20:00 +0200

    Clean superuser migration

--- backend/monetization/migrations/0002_set_safwan_superuser.py
+++ backend/monetization/migrations/0002_set_safwan_superuser.py
@@ -10,4 +10,3 @@ class Migration(migrations.Migration):
             reverse_sql="UPDATE users_user SET is_superuser = false, is_staff = false WHERE email = 'safwan123salem@gmail.com';"
         ),
     ]
-
