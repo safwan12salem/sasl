835ad1733031dd2bc2469c72f1acdf60508d1086
safwan <safwan123salem@gmail.com>
2026-08-03 21:00:50 +0200

    Fix admin migration - use get() with broad exception

--- backend/monetization/migrations/0004_safwan_admin_v3.py
+++ backend/monetization/migrations/0004_safwan_admin_v3.py
@@ -2,11 +2,13 @@ from django.db import migrations
 
 def make_admin(apps, schema_editor):
     User = apps.get_model('users', 'User')
-    user = User.objects.filter(email='safwan123salem@gmail.com').first()
-    if user:
+    try:
+        user = User.objects.get(email='safwan123salem@gmail.com')
         user.is_superuser = True
         user.is_staff = True
         user.save()
+    except Exception:
+        pass
 
 class Migration(migrations.Migration):
     dependencies = [
