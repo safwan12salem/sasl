7e76e4cb5c2d2b89eeea930da2d4dfb73c33e8f8
safwan12salem <safwan123salem@gmail.com>
2026-08-17 20:24:00 +0200

    Fix: add video WebSocket route to Tutoring

--- backend/tutoring/consumers.py
+++ backend/tutoring/consumers.py
@@ -5,6 +5,26 @@ ISOLATED - uses /ws/tutoring/{room_id}/
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 
+class TutoringVideoConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = f'video_{self.room_name}'
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {'type': 'relay_message', 'data': data}
+        )
+
+    async def relay_message(self, event):
+        await self.send(text_data=json.dumps(event['data']))
+
 
 class TutoringChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -33,3 +53,4 @@ class TutoringChatConsumer(AsyncWebsocketConsumer):
             'type': 'chat',
             'text': f"{event['sender']}: {event['text']}",
         }))
+
--- backend/tutoring/routing.py
+++ backend/tutoring/routing.py
@@ -1,6 +1,7 @@
 from django.urls import re_path
-from .consumers import TutoringChatConsumer
+from .consumers import TutoringChatConsumer, TutoringVideoConsumer
 
 websocket_urlpatterns = [
     re_path(r'ws/tutoring/(?P<room_id>[\w-]+)/$', TutoringChatConsumer.as_asgi()),
+    re_path(r'ws/video/(?P<room_name>[\w-]+)/$', TutoringVideoConsumer.as_asgi()),
 ]
